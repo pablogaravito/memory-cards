f0767f8701c8b577525022fc9fff746b2ffda9bd
Pablo <pablo.garavito@gmail.com>
2023-12-30 12:48:09 -0500

    cards are selected and shuffled twice

--- app.js
+++ app.js
@@ -1,121 +1,234 @@
-let turns = 0;
-let cardsLeft = -1;
-let cardList = [
-    'darkness', 
-    'double', 
-    'fairy',
-    'fighting',
-    'fire',
-    'grass',
-    'lightning',
-    'metal',
-    'psychic',
-    'water'
-]
 
-const pokemonTypesPath = 'res/pokemon-types/';
 
-let cardSet;
-let board = [];
-let rows = 4;
-let columns = 5; 
 
-let selectedCard1 = null;
-let selectedCard2 = null;
-
-const boardEle = document.querySelector('#board');
-
-window.onload = function() {
-    shuffleCards();
-    startGame();
-}
-
-function shuffleCards() {
-    cardSet = cardList.concat(cardList);
-    console.log(cardSet);
-
-    //shuffle
-    for (let i = 0; i < cardSet.length; i++) {
-        let j = Math.floor(Math.random() * cardSet.length);
-        let temp = cardSet[i];
-        cardSet[i] = cardSet[j];
-        cardSet[j] = temp;
+class AudioController {
+    constructor() {
+        // this.bgMusic = new Audio('res/Audio/creepy.mp3');
+        this.flipSound = new Audio('res/Audio/flip.wav');
+        this.matchSound = new Audio('res/Audio/match.wav');
+        this.victorySound = new Audio('res/Audio/victory.wav');
+        this.gameOverSound = new Audio('res/Audio/gameover.wav');
+        // this.bgMusic.volume = 0.3;
+        // this.bgMusic.loop = true;
     }
-    console.log(cardSet);
+    // startMusic() {
+    //     this.bgMusic.play();
+    // }
+    // stopMusic() {
+    //     this.bgMusic.pause();
+    //     this.bgMusic.currentTime = 0;
+    // }
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        // this.stopMusic();
+        this.victorySound.play();
+    }
+    gameOver() {
+        // this.stopMusic();
+        this.gameOverSound.play();
+    }
 }
 
-function startGame() {
-    cardsLeft = cardSet.length;
-    console.log('cardsLeft at start', cardsLeft);
-    //arrange the board
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++) {
-            let cardImg = cardSet.pop();
-            row.push(cardImg);
-
-            let card = document.createElement('img');
-            card.id = r.toString() + '-' + c.toString();
-            card.src = `${pokemonTypesPath}${cardImg}.jpg`;
-
-            card.addEventListener('click', selectCard);
-
-            //card.src = 'res/pokemon-types/' + cardImg + '.jpg';
-            card.classList.add('card');
-            boardEle.appendChild(card);
+class Game {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.querySelector('#time-remaining');
+        this.flips = document.querySelector('#flips');
+        this.audioController = new AudioController();
+    }
+    startGame() {
+        this.cardToCheck = null;
+        this.hideCards();
+        this.currentFlips = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+        console.log(this.selectCards(20));
+        setTimeout(() => {
+            // this.audioController.startMusic();
+            // this.shuffleCards();
+            this.countDown = this.startCountDown();
+            this.busy = false;
+        }, 500);
+    }
+    selectCards(totalCards) {
+        const xmasCards = [
+            'bell', 
+            'bells', 
+            'calendar',
+            'candles',
+            'champagne',
+            'dessert',
+            'gingerbread',
+            'gloves',
+            'hat',
+            'letter',
+            'ornament',
+            'ornament2',
+            'ornament3',
+            'ornament4',
+            'polar_bear',
+            'reindeer',
+            'santa_smile',
+            'santa_sunglasses',
+            'sled',
+            'snowglobe',
+            'snowglobe2',
+            'snowglobe3',
+            'snowman',
+            'sock',
+            'star',
+            'tree',
+            'tree2',
+            'wreath'
+        ];
+         let cardsNeeded = totalCards/2;
+        const shuffledCards = this.shuffleCards(xmasCards);
+        let cardList = shuffledCards.slice(shuffledCards.length - cardsNeeded);
+        let cardSet = cardList.concat(cardList);
+        return this.shuffleCards(cardSet);
+    }
+    shuffleCards(cardsArray) {
+        let newCardsArray = cardsArray.slice();
+        // for (let i = this.cardsArray.length - 1; i > 0; i--) {
+        //     let randIndex = Math.floor(Math.random() * (i+1));
+        //     this.cardsArray[randIndex].style.order = i;
+        //     this.cardsArray[i].style.order = randIndex;
+        // }
+        //modern Fisher Yates algorithm
+        let temp;
+        for (let i = newCardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i+1));
+            temp = newCardsArray[i];
+            newCardsArray[i] = newCardsArray[randIndex];
+            newCardsArray[randIndex] = temp;
         }
-        board.push(row);
+        return newCardsArray;
     }
-    setTimeout(hideCards, 1100);
-    
-}
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        });
+    };
+    startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if (this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+    gameOver() {
+        clearInterval(this.countDown);
+        this.audioController.gameOver();
+        document.querySelector('#game-over-text').classList.add('visible');
+        this.hideCards();
+    }
+    victory() {
+        clearInterval(this.countDown);
+        this.audioController.victory();
+        document.querySelector('#victory-text').classList.add('visible');
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.audioController.flip();
+            this.currentFlips++;
+            this.flips.innerText = this.currentFlips;
+            card.classList.add('visible');
 
-function hideCards() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
-            let card = document.getElementById(r.toString() + '-' + c.toString());
-            card.src = `${pokemonTypesPath}back.jpg`;
+            if (this.cardToCheck) 
+                this.checkForCardMatch(card);
+            else
+                this.cardToCheck = card;
         }
     }
-}
+    getCardType(card) {
+        return card.getElementsByClassName('card-value')[0].src;
+    }
+    checkForCardMatch(card) {
+        if (this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardMisMatch(card, this.cardToCheck);
 
-function selectCard() {
-    if (this.src.includes('back')) {
-        if (!selectedCard1) {
-            selectedCard1 = this;
-            let coords = selectedCard1.id.split('-');
-            let r = parseInt(coords[0]);
-            let c = parseInt(coords[1]);
-            
-            selectedCard1.src = `${pokemonTypesPath}${board[r][c]}.jpg`
-
-
-        } else if (this !== selectedCard1 && !selectedCard2) {
-            selectedCard2 = this;
-            let coords = selectedCard2.id.split('-');
-            let r = parseInt(coords[0]);
-            let c = parseInt(coords[1]);
-            
-            selectedCard2.src = `${pokemonTypesPath}${board[r][c]}.jpg`;
-            setTimeout(compareCards, 1000);
-        }
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        this.audioController.match();
+        if (this.matchedCards.length === this.cardsArray.length)
+            this.victory();
+    }
+    cardMisMatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);      
+    }
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
 }
 
-function compareCards() {
-    turns++;
-    const turnSpan = document.querySelector('#turns');
-    turnSpan.innerText = turns;
-    if (selectedCard1.src !== selectedCard2.src){
-        selectedCard1.src = `${pokemonTypesPath}back.jpg`;
-        selectedCard2.src = `${pokemonTypesPath}back.jpg`;     
-    }  else {
-        cardsLeft = cardsLeft - 2;
-    }
-    console.log('cardsLeft: ', cardsLeft);
-    if (cardsLeft === 0) {
-        console.log(`You win! Took you ${turns} turns`);
-    }
-    selectedCard1 = null;
-    selectedCard2 = null;
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    const board = document.querySelector('#board');
+    populateBoard(board);
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new Game(60, cards);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);
+        });
+    });
 }
+
+function populateBoard(board) {
+    for (let index = 1; index <= 20; index++) {
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+        //cardBack
+        const cardBack = document.createElement('div');
+        cardBack.classList.add('card-back');
+        cardBack.classList.add('card-face');
+        const imgBack = document.createElement('img');
+        imgBack.src = "res/xmas/0_card_back.png";
+        cardBack.appendChild(imgBack);
+        //cardFront
+        // const cardFront = document.createElement('div');
+        // cardFront.classList.add('card-front');
+        // cardFront.classList.add('card-face');
+        // const imgFront = document.createElement('img');
+        // imgFront.classList.add('card-value');
+        // imgFront.src = "res/pokemons/dragonite.png";
+        // cardFront.appendChild(imgFront);
+
+        card.appendChild(cardBack);
+        // card.appendChild(cardFront);
+        board.appendChild(card);
+    }
+}
+
+    ready();
